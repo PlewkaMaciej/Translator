4feb4edd325405acc072611a46d72117298f80d2
PlewkaMaciej <82128422+PlewkaMaciej@users.noreply.github.com>
2022-06-02 00:55:33 +0200

    get textarea value to state for translate it

--- src/components/TranlatedText.js
+++ src/components/TranlatedText.js
@@ -1,9 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../styles/translator.css";
-function TranslatedText(){
+function TranslatedText({translatedSentence}){
     return(
 <div className="translated-text-container">
 <p>Translated text in English:</p>
+<p>{translatedSentence}</p>
 </div>
     )
 }
--- src/components/Translator.js
+++ src/components/Translator.js
@@ -5,9 +5,8 @@ import "../styles/translator.css";
 import TextToTranslate from "./TextToTranslate";
 import TranslatedText from "./TranlatedText";
 function Translator() {
-    const [sentenceToTranslate, setSentenceToTranslate] = useState("hello");
-    const [chooseLanguange, setChooseLanguange] = useState("pl")
-    const [translatedSentence, setTranslatedSentence] = useState(null)
+    const [sentenceToTranslate, setSentenceToTranslate] = useState();
+    const [translatedSentence, setTranslatedSentence] = useState()
     // useEffect(() => {
     //     const encodedParams = new URLSearchParams();
     //     encodedParams.append("q", "Hello, world!");
@@ -37,14 +36,14 @@ function Translator() {
             </div>
             <div className="container-for-both-text-area">
                 <div className="container-translate-text">
-                    <TextToTranslate />
+                    <TextToTranslate setSentenceToTranslate={setSentenceToTranslate}/>
                 </div>
                 <div className="container-translate-text">
-                    <TranslatedText />
+                    <TranslatedText translatedSentence={translatedSentence} />
                 </div>
             </div>
             <div className="main-button-container">
-            <ButtonToTranslate />
+            <ButtonToTranslate sentenceToTranslate={sentenceToTranslate} setTranslatedSentence={setTranslatedSentence}/>
             </div>
             <p className="information-paragraph">Because I use a free api after entering 500 letters the limit is used</p>
         </div>
